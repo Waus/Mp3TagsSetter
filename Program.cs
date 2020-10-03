57444db0ebf8cea663fcd4dd41ed48f780ae5f5f
waus <kamil.muranski.official@gmail.com>
2020-10-03 23:39:36 +0200

    Albumy typu Split + poprawki

--- Program.cs
+++ Program.cs
@@ -31,27 +31,48 @@ namespace Mp3TagsSetter
                         string[] filePathSplitted = filePath.Split('\\');
                         var len = filePathSplitted.Length;
 
-                        var fileName = Path.GetFileNameWithoutExtension(filePath);
-                        var trackNumber = fileName.Substring(0, 2);
-                        var trackName = fileName.Substring(4);
-
                         var albumDirectoryNameTest = filePathSplitted[len - 2];
                         if (!albumDirectoryNameRegex.IsMatch(albumDirectoryNameTest))
                             len = len - 1; //patrzymy poziom wyżej, bo w albumie mogły być np. 2 CD w osobnych folderach
 
-                        var albumDirectoryName = filePathSplitted[len - 2];
-                        var albumYear = albumDirectoryName.Substring(0, 4);
-                        var albumName = albumDirectoryName.Substring(6);
+                        var fileName = "";
+                        var trackNumber = "";
+                        var trackName = "";
+                        var albumDirectoryName = "";
+                        var albumYear = "";
+                        var albumName = "";
+                        var artist = "";
+                        var genre = "";
 
-                        var artist = filePathSplitted[len - 3];
-                        var genre = filePathSplitted[len - 4];
+                        fileName = Path.GetFileNameWithoutExtension(filePath);
+
+                        bool isAlbumSplit = albumDirectoryNameTest.Contains("(Split)");
+
+                        if (!isAlbumSplit)
+                        {
+                            trackName = fileName.Substring(5);
+                            artist = filePathSplitted[len - 3];
+                        }
+                        else //np. "08 - Malum - Desecrating the False Temples"
+                        {
+                            var index = fileName.IndexOf('-', fileName.IndexOf('-') + 1);
+                            trackName = fileName.Substring(index + 2); // 2 znaki po drugim myślniku
+                            artist = fileName.Substring(5, index - 6); // między pierwszym a drugim myślnikiem
+                        }
+
+                        trackNumber = fileName.Substring(0, 2);
+
+                        albumDirectoryName = filePathSplitted[len - 2];
+                        albumYear = albumDirectoryName.Substring(0, 4);
+                        albumName = albumDirectoryName.Substring(7);
+
+                        genre = filePathSplitted[len - 4];
 
                         string[] artists = new string[1];
                         artists[0] = artist;
                         string[] genres = new string[1];
                         genres[0] = genre;
 
-                        //taglib-sharp
                         var file = TagLib.File.Create(filePath);
 
                         file.Tag.Track = Convert.ToUInt32(trackNumber);
