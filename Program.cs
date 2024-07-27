b42f103229080cb55f57c79357bb1bc44a7218e6
Kamil Murański <kamil.muranski.official@gmail.com>
2024-07-27 22:35:13 +0200

    Jednak wycofanie MusicRenamera, budowanie do 1 pliku exe

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
-using TagLib;
 
 namespace Mp3TagsSetter
 {
@@ -19,7 +18,6 @@ namespace Mp3TagsSetter
         [STAThread]
         static void Main()
         {
-            RenameFiles();
             CleanFolderNames();
             SetFolderNames();
             SetMp3Tags();
@@ -209,87 +207,6 @@ namespace Mp3TagsSetter
             }
             MessageBox.Show("Zakończono przetwarzanie", "Informacja");
         }
-
-        public static void RenameFiles() // to format "02 - Hey hey Oh!"
-        {
-            Regex regex1 = new Regex(@"^\d\d\s[\w* [(\.]+");     //02 Hey hey Oh!
-            Regex regex2 = new Regex(@"^\d\d\.\s[\w* [(\.]+");        //02. Hey hey Oh!
-            Regex regex3 = new Regex(@"^\d\d\-[\w* [(\.]+");        //02-Hey hey Oh!
-            Regex regex4 = new Regex(@"^\d\d\-\s[\w* [(\.]+");        //02- Hey hey Oh!
-            Regex regex5 = new Regex(@"^\d\d\.[\w* [(\.]+");        //02.Hey hey Oh!
-
-            Regex regex1a = new Regex(@"^\d\s[\w* [(\.]+");     //2 Hey hey Oh!
-            Regex regex2b = new Regex(@"^\d\.\s[\w* [(\.]+");        //2. Hey hey Oh!
-            Regex regex3c = new Regex(@"^\d\-[\w* [(\.]+");        //2-Hey hey Oh!
-            Regex regex4d = new Regex(@"^\d\-\s[\w* [(\.]+");        //2- Hey hey Oh!
-            Regex regex5e = new Regex(@"^\d\.[\w* [(\.]+");        //2.Hey hey Oh!
-
-            string path = Directory.GetCurrentDirectory();
-            foreach (string filePath in Directory.GetFiles(path, "*", SearchOption.AllDirectories))
-            {
-                var fileExtension = Path.GetExtension(filePath);
-                if (fileExtension == ".mp3")
-                {
-                    string fileName = Path.GetFileName(filePath);
-                    string pathWithoutName = filePath.Replace(fileName, "");
-                    string fileNameRenamed = "";
-
-                    if (regex1.IsMatch(fileName))
-                    {
-                        fileNameRenamed = fileName.Substring(0, 2) + " - " + fileName.Substring(3, fileName.Length - 3);
-                    }
-                    else if (regex2.IsMatch(fileName))
-                    {
-                        fileNameRenamed = fileName.Substring(0, 2) + " - " + fileName.Substring(4, fileName.Length - 4);
-                    }
-                    else if (regex3.IsMatch(fileName))
-                    {
-                        fileNameRenamed = fileName.Substring(0, 2) + " - " + fileName.Substring(3, fileName.Length - 3);
-                    }
-                    else if (regex4.IsMatch(fileName))
-                    {
-                        fileNameRenamed = fileName.Substring(0, 2) + " - " + fileName.Substring(4, fileName.Length - 4);
-                    }
-                    else if (regex5.IsMatch(fileName))
-                    {
-                        fileNameRenamed = fileName.Substring(0, 2) + " - " + fileName.Substring(3, fileName.Length - 3);
-                    }
-                    else if (regex1a.IsMatch(fileName))
-                    {
-                        fileNameRenamed = "0" + fileName.Substring(0, 1) + " - " + fileName.Substring(2, fileName.Length - 2);
-                    }
-                    else if (regex2b.IsMatch(fileName))
-                    {
-                        fileNameRenamed = "0" + fileName.Substring(0, 1) + " - " + fileName.Substring(3, fileName.Length - 3);
-                    }
-                    else if (regex3c.IsMatch(fileName))
-                    {
-                        fileNameRenamed = "0" + fileName.Substring(0, 1) + " - " + fileName.Substring(2, fileName.Length - 2);
-                    }
-                    else if (regex4d.IsMatch(fileName))
-                    {
-                        fileNameRenamed = "0" + fileName.Substring(0, 1) + " - " + fileName.Substring(3, fileName.Length - 3);
-                    }
-                    else if (regex5e.IsMatch(fileName))
-                    {
-                        fileNameRenamed = "0" + fileName.Substring(0, 1) + " - " + fileName.Substring(2, fileName.Length - 2);
-                    }
-
-                    if (!String.IsNullOrEmpty(fileNameRenamed))
-                    {
-                        string filePathRenamed = pathWithoutName + fileNameRenamed;
-                        string oldFileNameWithPath = pathWithoutName + fileName;
-                        try
-                        {
-                            System.IO.File.Move(oldFileNameWithPath, filePathRenamed);
-                        }
-                        catch
-                        {
-                        }
-                    }
-                }
-            }
-        }
     }
 
     public class FileData
@@ -300,6 +217,5 @@ namespace Mp3TagsSetter
         public string albumName { get; set; }
         public string[] artists { get; set; }
         public string[] genres { get; set; }
-        public string folderTags { get; set; }  // Added for storing tags that should appear in the folder name
     }
 }
