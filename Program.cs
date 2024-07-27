c04e46a86bc011f32457eb9b19ed72fd31098a6d
Kamil Murański <kamil.muranski.official@gmail.com>
2024-07-27 21:51:10 +0200

    dodanie funkcjonalności MusicRenamera

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace Mp3TagsSetter
         [STAThread]
         static void Main()
         {
+            RenameFiles();
             CleanFolderNames();
             SetFolderNames();
             SetMp3Tags();
@@ -43,7 +44,7 @@ namespace Mp3TagsSetter
                         Directory.Move(dir, newDirPath);
                     }
                 }
-                catch (Exception ignored)
+                catch (Exception)
                 {
                 }
             }
@@ -84,7 +85,7 @@ namespace Mp3TagsSetter
                             Path.GetFileName(albumFolderPath);
                         }
                     }
-                    catch (Exception ignored)
+                    catch (Exception)
                     {
                     }
 
@@ -201,12 +202,93 @@ namespace Mp3TagsSetter
 
                         tagFile.Save();
                     }
-                    catch (Exception ignored)
+                    catch (Exception)
                     {
                     }
                 }
             }
             MessageBox.Show("Zakończono przetwarzanie", "Informacja");
+        }
+
+        public static void RenameFiles() // to format "02 - Hey hey Oh!"
+        {
+            Regex regex1 = new Regex(@"^\d\d\s[\w* [(\.]+");     //02 Hey hey Oh!
+            Regex regex2 = new Regex(@"^\d\d\.\s[\w* [(\.]+");        //02. Hey hey Oh!
+            Regex regex3 = new Regex(@"^\d\d\-[\w* [(\.]+");        //02-Hey hey Oh!
+            Regex regex4 = new Regex(@"^\d\d\-\s[\w* [(\.]+");        //02- Hey hey Oh!
+            Regex regex5 = new Regex(@"^\d\d\.[\w* [(\.]+");        //02.Hey hey Oh!
+
+            Regex regex1a = new Regex(@"^\d\s[\w* [(\.]+");     //2 Hey hey Oh!
+            Regex regex2b = new Regex(@"^\d\.\s[\w* [(\.]+");        //2. Hey hey Oh!
+            Regex regex3c = new Regex(@"^\d\-[\w* [(\.]+");        //2-Hey hey Oh!
+            Regex regex4d = new Regex(@"^\d\-\s[\w* [(\.]+");        //2- Hey hey Oh!
+            Regex regex5e = new Regex(@"^\d\.[\w* [(\.]+");        //2.Hey hey Oh!
+
+            string path = Directory.GetCurrentDirectory();
+            foreach (string filePath in Directory.GetFiles(path, "*", SearchOption.AllDirectories))
+            {
+                var fileExtension = Path.GetExtension(filePath);
+                if (fileExtension == ".mp3")
+                {
+                    string fileName = Path.GetFileName(filePath);
+                    string pathWithoutName = filePath.Replace(fileName, "");
+                    string fileNameRenamed = "";
+
+                    if (regex1.IsMatch(fileName))
+                    {
+                        fileNameRenamed = fileName.Substring(0, 2) + " - " + fileName.Substring(3, fileName.Length - 3);
+                    }
+                    else if (regex2.IsMatch(fileName))
+                    {
+                        fileNameRenamed = fileName.Substring(0, 2) + " - " + fileName.Substring(4, fileName.Length - 4);
+                    }
+                    else if (regex3.IsMatch(fileName))
+                    {
+                        fileNameRenamed = fileName.Substring(0, 2) + " - " + fileName.Substring(3, fileName.Length - 3);
+                    }
+                    else if (regex4.IsMatch(fileName))
+                    {
+                        fileNameRenamed = fileName.Substring(0, 2) + " - " + fileName.Substring(4, fileName.Length - 4);
+                    }
+                    else if (regex5.IsMatch(fileName))
+                    {
+                        fileNameRenamed = fileName.Substring(0, 2) + " - " + fileName.Substring(3, fileName.Length - 3);
+                    }
+                    else if (regex1a.IsMatch(fileName))
+                    {
+                        fileNameRenamed = "0" + fileName.Substring(0, 1) + " - " + fileName.Substring(2, fileName.Length - 2);
+                    }
+                    else if (regex2b.IsMatch(fileName))
+                    {
+                        fileNameRenamed = "0" + fileName.Substring(0, 1) + " - " + fileName.Substring(3, fileName.Length - 3);
+                    }
+                    else if (regex3c.IsMatch(fileName))
+                    {
+                        fileNameRenamed = "0" + fileName.Substring(0, 1) + " - " + fileName.Substring(2, fileName.Length - 2);
+                    }
+                    else if (regex4d.IsMatch(fileName))
+                    {
+                        fileNameRenamed = "0" + fileName.Substring(0, 1) + " - " + fileName.Substring(3, fileName.Length - 3);
+                    }
+                    else if (regex5e.IsMatch(fileName))
+                    {
+                        fileNameRenamed = "0" + fileName.Substring(0, 1) + " - " + fileName.Substring(2, fileName.Length - 2);
+                    }
+
+                    if (!String.IsNullOrEmpty(fileNameRenamed))
+                    {
+                        string filePathRenamed = pathWithoutName + fileNameRenamed;
+                        string oldFileNameWithPath = pathWithoutName + fileName;
+                        try
+                        {
+                            System.IO.File.Move(oldFileNameWithPath, filePathRenamed);
+                        }
+                        catch
+                        {
+                        }
+                    }
+                }
+            }
         }
     }
 
