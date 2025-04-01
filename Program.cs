676c5660455c075c7b614e654941caec0f0010e5
Kamil Murański <kamil.muranski.official@gmail.com>
2025-04-01 18:51:20 +0200

    Grafika albumu dodana do metadanych

--- Program.cs
+++ Program.cs
@@ -3,14 +3,15 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using TagLib;
 
 namespace Mp3TagsSetter
 {
     static class Program
     {
-        // Regex to check if the directory name is an album name
+        // Regex do sprawdzenia, czy nazwa folderu to album
         static Regex albumDirectoryNameRegex = new Regex(@"^\d{4}\s\-\s[\w* [(\.]+"); // "2020 - Tytuł"
-        // Regex to remove "Compilation", "Single", "Live", "Split" from album tag and picture name
+        // Regex do usunięcia "(Compilation|Single|Live|Split|2CD|...)" z nazwy albumu
         static Regex folderNameKeepTagsRegex = new Regex(@"\s*\((Compilation|Single|Live|Split|2CD|3CD|4CD|5CD)\)$");
 
         [STAThread]
@@ -23,6 +24,7 @@ namespace Mp3TagsSetter
         {
             string path = Directory.GetCurrentDirectory();
             var allFiles = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+
             foreach (string filePath in allFiles)
             {
                 var fileExtension = Path.GetExtension(filePath);
@@ -30,20 +32,25 @@ namespace Mp3TagsSetter
                 {
                     try
                     {
-                        // Make the file writable
-                        System.IO.File.SetAttributes(filePath, System.IO.File.GetAttributes(filePath) & ~FileAttributes.ReadOnly);
+                        // Usunięcie atrybutu tylko do odczytu, aby móc zapisać zmiany
+                        System.IO.File.SetAttributes(
+                            filePath,
+                            System.IO.File.GetAttributes(filePath) & ~FileAttributes.ReadOnly
+                        );
 
                         string[] filePathSplitted = filePath.Split(Path.DirectorySeparatorChar);
                         var len = filePathSplitted.Length;
 
+                        // Sprawdzamy, czy folder nazwy albumu wygląda na wzorzec "2020 - Nazwa"
                         var albumFolderName = filePathSplitted[len - 2];
                         var albumDirectoryHasFoldersWithCds = albumDirectoryNameRegex.IsMatch(albumFolderName);
+
                         if (!albumDirectoryHasFoldersWithCds)
                         {
-                            len = len - 1; // Look one level up if the album folder might contain multiple CDs
+                            // Jeżeli mamy np. folder "CD1" lub "CD2", musimy spojrzeć wyżej
+                            len = len - 1;
                             albumFolderName = filePathSplitted[len - 2];
                         }
-                            
 
                         var fileName = Path.GetFileNameWithoutExtension(filePath);
 
@@ -53,33 +60,51 @@ namespace Mp3TagsSetter
                             genres = new string[1]
                         };
 
+                        // Czy to jest split?
                         bool isAlbumSplit = albumFolderName.Contains("(Split)");
 
                         if (!isAlbumSplit)
                         {
+                            // Nazwa utworu po "XX - "
                             fileData.trackName = fileName.Substring(5);
+                            // Nazwa artysty to katalog wyżej
                             fileData.artists[0] = filePathSplitted[len - 3];
                         }
-                        else // e.g., "08 - Malum - Desecrating the False Temples"
+                        else
                         {
+                            // Jeżeli jest "08 - Malum - Desecrating the False Temples"
                             var index = fileName.IndexOf('-', fileName.IndexOf('-') + 1);
-                            fileData.trackName = fileName.Substring(index + 2); // 2 characters after the second hyphen
-                            fileData.artists[0] = fileName.Substring(5, index - 6); // Between the first and second hyphens
+                            fileData.trackName = fileName.Substring(index + 2);
+                            fileData.artists[0] = fileName.Substring(5, index - 6);
                         }
 
+                        // Numer utworu
                         fileData.trackNumber = Convert.ToUInt32(fileName.Substring(0, 2));
 
+                        // Rok i surowa nazwa albumu
                         var albumDirectoryName = filePathSplitted[len - 2];
                         fileData.albumYear = Convert.ToUInt32(albumDirectoryName.Substring(0, 4));
 
+                        // Usuwamy np. "(2CD)" z nazwy
                         var folderTagsMatch = folderNameKeepTagsRegex.Match(albumDirectoryName.Substring(7));
-
                         var correctAlbumName = folderTagsMatch.Success
-                            ? albumDirectoryName.Substring(7).Replace(folderTagsMatch.ToString(), string.Empty).Trim()
+                            ? albumDirectoryName.Substring(7)
+                                .Replace(folderTagsMatch.ToString(), string.Empty)
+                                .Trim()
                             : albumDirectoryName.Substring(7);
 
-                        string albumDirectoryNamePath = string.Join(Path.DirectorySeparatorChar.ToString(), filePathSplitted.Take(len - 1));
-                        string[] cdFoldersInAlbumFolder = System.IO.Directory.GetDirectories(albumDirectoryNamePath, "*.*", SearchOption.TopDirectoryOnly);
+                        // Ścieżka do folderu albumu (bądź rodzica "CD1" itp.)
+                        string albumDirectoryNamePath = string.Join(
+                            Path.DirectorySeparatorChar.ToString(),
+                            filePathSplitted.Take(len - 1)
+                        );
+
+                        // Czy album posiada podfoldery (np. CD1, CD2)?
+                        string[] cdFoldersInAlbumFolder = Directory.GetDirectories(
+                            albumDirectoryNamePath,
+                            "*.*",
+                            SearchOption.TopDirectoryOnly
+                        );
 
                         if (cdFoldersInAlbumFolder.Length == 0)
                         {
@@ -87,18 +112,19 @@ namespace Mp3TagsSetter
                         }
                         else
                         {
-                            {
-                                var cdAlbumFolderName = filePathSplitted[len - 1];
-                                var correctCdAlbumName = correctAlbumName + " - " + cdAlbumFolderName;
-                                fileData.albumName = correctCdAlbumName;
-                            }
+                            // Jeśli mamy foldery CD1, CD2, to w nazwie albumu dopisujemy " - CD1" itd.
+                            var cdAlbumFolderName = filePathSplitted[len - 1];
+                            var correctCdAlbumName = correctAlbumName + " - " + cdAlbumFolderName;
+                            fileData.albumName = correctCdAlbumName;
                         }
 
+                        // Nazwa gatunku (np. "Folk metal") z katalogu wyżej
                         fileData.genres[0] = filePathSplitted[len - 4];
 
-                        // Use TagLib.File with full qualification
+                        // Tworzymy obiekt TagLib do aktualnego pliku
                         TagLib.File tagFile = TagLib.File.Create(filePath);
 
+                        // Ustawiamy podstawowe tagi
                         tagFile.Tag.Track = fileData.trackNumber;
                         tagFile.Tag.Title = fileData.trackName;
                         tagFile.Tag.Year = fileData.albumYear;
@@ -107,14 +133,81 @@ namespace Mp3TagsSetter
                         tagFile.Tag.Performers = fileData.artists;
                         tagFile.Tag.Genres = fileData.genres;
 
+                        // Znajdź plik graficzny w folderze albumDirectoryNamePath
+                        string coverPath = FindCoverImageInFolder(albumDirectoryNamePath);
+
+                        if (!string.IsNullOrEmpty(coverPath) && System.IO.File.Exists(coverPath))
+                        {
+                            // Wczytujemy plik jako Tablicę bajtów
+                            byte[] coverBytes = System.IO.File.ReadAllBytes(coverPath);
+
+                            // Możemy spróbować ustalić MIME Type na podstawie rozszerzenia
+                            // (ew. można korzystać z biblioteki do rozpoznawania MIME z nagłówka)
+                            string mimeType = GetMimeTypeForImage(coverPath);
+
+                            TagLib.IPicture coverPicture = new TagLib.Picture
+                            {
+                                Data = coverBytes,
+                                Type = PictureType.FrontCover,
+                                Description = "Cover",
+                                MimeType = mimeType
+                            };
+
+                            // Ustawiamy obrazek jako jedyny w tagach
+                            tagFile.Tag.Pictures = new IPicture[] { coverPicture };
+                        }
+
                         tagFile.Save();
                     }
                     catch (Exception)
                     {
+                        // Ewentualny log błędu
                     }
                 }
             }
+
             MessageBox.Show("Zakończono przetwarzanie", "Informacja");
+        }
+
+        /// <summary>
+        /// Funkcja pomocnicza – szuka pierwszego pliku graficznego we wskazanym folderze (jpg/png/gif/bmp, itp.)
+        /// Zwraca pełną ścieżkę do pliku lub pusty string, jeśli nie znalazła.
+        /// </summary>
+        private static string FindCoverImageInFolder(string folderPath)
+        {
+            // Najpopularniejsze rozszerzenia obrazów
+            var imageExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif", ".bmp" };
+
+            // Znajdź wszystkie pliki w folderze, filtrując po rozszerzeniach
+            var imageFiles = Directory.EnumerateFiles(folderPath, "*.*", SearchOption.TopDirectoryOnly)
+                                      .Where(file => imageExtensions.Contains(Path.GetExtension(file).ToLower()))
+                                      .ToList();
+
+            // Założenie: w każdym folderze jest dokładnie 1 plik graficzny z okładką
+            // Zwracamy pierwszy z nich, jeśli istnieje
+            return imageFiles.FirstOrDefault() ?? string.Empty;
+        }
+
+        /// <summary>
+        /// Pomocnicza metoda ustalająca MIME type na podstawie rozszerzenia pliku.
+        /// </summary>
+        private static string GetMimeTypeForImage(string imagePath)
+        {
+            string ext = Path.GetExtension(imagePath).ToLower();
+            switch (ext)
+            {
+                case ".jpg":
+                case ".jpeg":
+                    return "image/jpeg";
+                case ".png":
+                    return "image/png";
+                case ".gif":
+                    return "image/gif";
+                case ".bmp":
+                    return "image/bmp";
+                default:
+                    return "image/unknown";
+            }
         }
     }
 
